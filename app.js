2546e52d335acba68ed3b65eb69b5cc11d105b7c
nouraAlthobaiti <48947272+nouraAlthobaiti@users.noreply.github.com>
2019-12-05 01:09:27 +0300

    all works unless pricing check pasing buttons! :)

--- app.js
+++ app.js
@@ -1,44 +1,54 @@
 //jshint esversion:6
+
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const _ = require("lodash");     //working with arrays, numbers, objects, strings, etc.
-const bcrypt = require('bcrypt');  //securety
+const _ = require("lodash"); //working with arrays, numbers, objects, strings, etc.
+const bcrypt = require('bcrypt'); //securety
 const app = express();
 const session = require('express-session');
 const MongoStore = require('connect-mongo')(session);
+//const alert = require("alert-node");   :((
 app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(express.static("public"));
+
 //--------------------------------------------
 //app.engine('html', require('ejs').renderFile);
 //app.set('view engine', 'html');
 //--------------------------------------------
 // parse incoming requests
-//app.use(bodyParser.json());     // tells the system that you want json to be used.
+app.use(bodyParser.json()); // tells the system that you want json to be used.
 
-mongoose.connect("mongodb://localhost:27017/testForAuth", {useUnifiedTopology:true , useNewUrlParser:true , useFindAndModify: false});
+mongoose.connect("mongodb://localhost:27017/testForAuth", {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
 
 var db = mongoose.connection;
 
 //Check connection
 db.once('open', function() {
- console.log('Connected to mongoDB');
+  console.log('Connected to mongoDB');
 });
 //Check for DB error
 db.on('error', function(err) {
- console.log(err);});
- //_______________________________________________________________________________session
+  console.log(err);
+});
+//_______________________________________________________________________________session
 
- //use sessions for tracking logins
- app.use(session({
-   secret: 'work hard',
-   resave: true,
-   saveUninitialized: false,
-   store: new MongoStore({
-     mongooseConnection: db
-   })
- }));
+//use sessions for tracking logins
+app.use(session({
+  secret: 'work hard',
+  resave: true,
+  saveUninitialized: false,
+  store: new MongoStore({
+    mongooseConnection: db
+  })
+}));
 //_______________________________________________________________________________schema
 // user email username password
 var UserSchema = new mongoose.Schema({
@@ -58,7 +68,7 @@ var UserSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
-  pricing: {
+  membership: {
     type: Number,
     required: true,
   }
@@ -67,9 +77,11 @@ var UserSchema = new mongoose.Schema({
 //__________________________________________________________________________________collection
 
 //authenticate input against database
-UserSchema.statics.authenticate = function (email, password, callback) {
-  User.findOne({ email: email })
-    .exec(function (err, user) {
+UserSchema.statics.authenticate = function(email, password, callback) {
+  User.findOne({
+      email: email
+    })
+    .exec(function(err, user) {
       if (err) {
         return callback(err);
       } else if (!user) {
@@ -77,7 +89,7 @@ UserSchema.statics.authenticate = function (email, password, callback) {
         err.status = 401;
         return callback(err);
       }
-      bcrypt.compare(password, user.password, function (err, result) {
+      bcrypt.compare(password, user.password, function(err, result) {
         if (result === true) {
           return callback(null, user);
         } else {
@@ -88,9 +100,9 @@ UserSchema.statics.authenticate = function (email, password, callback) {
 };
 
 //hashing a password before saving it to the database
-UserSchema.pre('save', function (next) {
+UserSchema.pre('save', function(next) {
   var user = this;
-  bcrypt.hash(user.password, 10, function (err, hash) {
+  bcrypt.hash(user.password, 10, function(err, hash) {
     if (err) {
       return next(err);
     }
@@ -104,48 +116,73 @@ var User = mongoose.model('User', UserSchema);
 
 //_______________________________________________________________________________routes main
 
+// send home file to this url
+app.get('/', function(req, res, next) {
+  return res.render("index");
+
+});
+//_______________________________________________________________________________routes sign in
 // send login file to this url
-app.get('/', function (req, res, next) {
- res.sendFile(__dirname + '/login.html');
-});
+app.get('/login', function(req, res, next) {
+  //return res.render("login");
+  // any ejs var should be given value (initiated)
+  return res.render("login", {
+    errmail: ''
+  });
 
+  //res.sendFile(__dirname + '/login.html');
+});
 //_______________________________________________________________________________routes login->pricing
 
 // send pricing file to this url
-app.get('/pricing', function (req, res, next) {
- res.sendFile(__dirname + '/pricing.html');
+app.get('/pricing', function(req, res, next) {
+  return res.render("pricing");
 });
 
-//_______________________________________________________________________________routes login->pricing
+//_______________________________________________________________________________routes signup->pricing
 
 // send pricing file to this url
-app.get('/signup', function (req, res, next) {
- res.sendFile(__dirname + '/signup.html');
+app.get('/signup', function(req, res, next) {
+  return res.render("signup", {
+    repeatedAccMsg: ''
+  });
+
+  //res.sendFile(__dirname + '/signup.html');
 });
 
 //_______________________________________________________________________________sign in
-app.post('/login', function (req, res, next) {
-if (req.body.logemail && req.body.logpassword) {
-  User.authenticate(req.body.logemail, req.body.logpassword, function (error, user) {
+app.post('/login', function(req, res, next) {
+  if (req.body.logemail && req.body.logpassword) {
+    User.authenticate(req.body.logemail, req.body.logpassword, function(error, user) {
+      //  req.body.errmail.style.visibility="hidden";
       if (error || !user) {
-      var err = new Error('Wrong email or password.');
-      err.status = 401;
-      return next(err);
-    } else {
-      req.session.userId = user._id;
-      return res.redirect('/profile');
-    }
-  });
-} else {
-  var err1 = new Error('All fields required.');
-  err1.status = 400;
-  return next(err1);
-}
-});
+        var err = new Error('Wrong email or password.');
 
+        return res.render("login", {
+          errmail: 'البريد الالكتروني أو كلمة المرور خاطئة'
+        });
+      } else {
+        req.session.userId = user._id;
+        return res.redirect('/profile');
+      }
+    });
+  } else {
+    var err1 = new Error('All fields required.');
+    err1.status = 400;
+    return next(err1);
+  }
+});
+//err.status = 401;
+//res.send('Wrong email or password.');  //new page
+//alert("Wrong email or password"); //nice box :( 'alert-node'
+//  document.getElementById('errmsg').style.visibility = "visible";
+//req.body.errmail.textContent="البريد الالكتروني أو كلمة المرور خاطئة  ";
+//window.alert('message');
+//alertMessage('message');
+//res.send(alert(err));
 
 //_______________________________________________________________________________sign up
-app.post('/signup', function (req, res, next) {
+app.post('/signup', function(req, res, next) {
 
   // confirm that user typed same password twice
   if (req.body.password !== req.body.passwordConf) {
@@ -154,38 +191,46 @@ app.post('/signup', function (req, res, next) {
     res.send("passwords dont match :.( ");
     return next(err);
   }
-//if they all entered <sign up>
-  if (req.body.email &&
+
+  //if they all entered <sign up>
+  /*if (req.body.email &&
     req.body.username &&
     req.body.password &&
     req.body.passwordConf) {
+*/
+  //create var to make doc
+  var userData = {
+    email: req.body.email,
+    username: req.body.username,
+    password: req.body.password,
+    membership: 0
+  };
 
-//create var to make doc
-    var userData = {
-      email: req.body.email,
-      username: req.body.username,
-      password: req.body.password,
-    };
+  //create doc  ///repeatedAccMsg
+  User.create(userData, function(error, user) {
+    if (error) {
+      return res.render("signup", {
+        repeatedAccMsg: "هذا الايميل مستخدم !"
+      });
+    } else {
 
-//create doc
-    User.create(userData, function (error, user) {
-      if (error) {
-        return next(error);
-      } else {
+
+      User.authenticate(req.body.email, req.body.password, function(error, user) {
         req.session.userId = user._id;
-        res.redirect('/profile');
-      }
-    });
-  }
+        return res.redirect('/profile');
+      });
+    }
+  });
+  //} //end if
 
 });
 //_______________________________________________________________________________profile
 
 
 // GET route after registering
-app.get('/profile', function (req, res, next) {
+app.get('/profile', function(req, res, next) {
   User.findById(req.session.userId)
-    .exec(function (error, user) {
+    .exec(function(error, user) {
       if (error) {
         return next(error);
       } else {
@@ -194,7 +239,10 @@ app.get('/profile', function (req, res, next) {
           err.status = 400;
           return next(err);
         } else {
-          return res.render("profile" , {username: user.username , email:user.email}); //(/profile --> change url only) otherwise (render my ejs file)
+          return res.render("profile", {
+            username: user.username,
+            email: user.email
+          }); //(/profile --> change url only) otherwise (render my ejs file)
         }
       }
     });
@@ -203,10 +251,10 @@ app.get('/profile', function (req, res, next) {
 //_______________________________________________________________________________loguot
 
 // GET for logout logout
-app.get('/logout', function (req, res, next) {
+app.get('/logout', function(req, res, next) {
   if (req.session) {
     // delete session object
-    req.session.destroy(function (err) {
+    req.session.destroy(function(err) {
       if (err) {
         return next(err);
       } else {
@@ -220,32 +268,33 @@ app.get('/logout', function (req, res, next) {
 //_______________________________________________________________________________pricing
 
 // GET for logout logout
-app.post('/pricingCheck', function (req, res, next) {
-  var pricvar=req.body.this.name;
-//check login
-if(req.session){
+app.post('/pricingCheck', function(req, res, next) {
+  //var pricvar = req.body.name; not working
+//membersh
+    //check login
+  if (req.session.userId != null) {
 
-  User.findOneAndUpdate({_id:req.session.userId}, {$pull: {pricing: pricvar}}, (function (err) {
-    if (err) {
-      return next(err);
-    } else {
-      return res.redirect('/');
-     }
-   })
- );
-}
+    User.findOneAndUpdate({_id: req.session.userId},
+       { membership: membersh }, (function(err) {
+         if (err) {
+        return next(err);
 
-else {
-     // go login
-     return res.redirect('/login');
-}
+      } else {
+        return res.redirect('/');
+      }
+    }));
+
+  } else {
+    // go login
+    return res.redirect('/login');
+  }
 
 });
 
 //_______________________________________________________________________________server port
 
 
-    //server port
+//server port
 app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
