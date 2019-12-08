b39e9492ae0430eb28b93c2f03f3c9bd8a304fca
Wafaa-ALHakami <55312049+Wafaa-ALHakami@users.noreply.github.com>
2019-12-08 15:15:26 +0300

    change updates name

--- app.js
+++ app.js
@@ -631,7 +631,7 @@ app.post('/updateAlbum', function(req, res, next) {
 
 
   //update
-  if (req.body.title != null) {
+  if (req.body.updateTitle != null) {
     Albums.findOneAndUpdate({ ownerName: req.session.userName, _id:req.params.id}, { title: req.body.title },
 
       (function(err)
